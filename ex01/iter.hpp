818cbedd087130d31414fd5a651777aa152388ef
hmouis <houdayfamouis@gmail.com>
2026-04-20 19:04:37 +0100

    add ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,25 +3,13 @@
 #include <iostream>
 
 template <typename t>
-void f(t &e){
+void func(t &e){
     std::cout << "element = " << e << std::endl;
 }
 
-template <typename t>
-void cf(t const &e){
-    std::cout << "const element = " << e << std::endl;
-}
-
-template <typename t>
-void iter(t *arr, size_t size, void (*f)(t&))
+template <typename t, typename f>
+void iter(t *arr, size_t size, f func)
 {
     for (size_t i = 0; i < size; i++)
-        f(arr[i]);
-}
-
-template <typename t>
-void iter(t *arr, size_t size, void (*cf)(const t&))
-{
-    for (size_t i = 0; i < size; i++)
-        cf(arr[i]);
+        func(arr[i]);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,8 +6,8 @@ int main()
     int const iarr[4] = {1, 2, 3, 4};
     
     std::cout << "Testing array of strings \n";
-    ::iter(sarr, 3, ::f<std::string>);
+    ::iter(sarr, 3, ::func<std::string>);
 
     std::cout << "Testing array of const int \n";
-    ::iter(iarr, 4, ::cf<int>);
+    ::iter(iarr, 4, ::func<int const>);
 }
